7a7739bf3960e59b64c4946046ae75264d2f361d
itcast-hsian <43436182+itcast-hsian@users.noreply.github.com>
2019-07-16 17:32:06 +0800

    计算相隔时间

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -53,7 +53,7 @@ export default {
             url: "/airs",
             params: this.$route.query
         }).then(res => {
-            // console.log(res.data)
+            console.log(res.data)
             this.flightsData = res.data;
         })
     }
