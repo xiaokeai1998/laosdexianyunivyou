d73d69df689d4e6eaaa4c4d72dd2f3e72efa244e
itcast-hsian <43436182+itcast-hsian@users.noreply.github.com>
2019-07-19 16:53:59 +0800

    拦截未授权的请求

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,7 +3,7 @@ import { Message } from "element-ui";
 
 // axios拦截器
 // 第一个参数是nuxt对象
-export default ( { $axios } ) => {
+export default ( { $axios, redirect } ) => {
 
     // 拦截axios的错误请求
     $axios.onError(err => {
@@ -12,6 +12,12 @@ export default ( { $axios } ) => {
         if(statusCode === 400){
             // 错误提示
             Message.error(message);
+        }
+
+        // 未授权
+        if( statusCode === 401 || statusCode === 403 ){
+            // 跳转到登录页
+            redirect("/user/login");
         }
     });
 }
